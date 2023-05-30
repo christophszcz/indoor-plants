e5c82675bb2eba22caf0743bf1444ee9b27d3857
Chris Szczechowicz <info@cjszczechowicz.com>
2023-05-29 22:55:20 -0400

    Add the native mobile platforms

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.christophszcz.indoorPlants',
   appName: 'indoor plants',
-  webDir: 'dist',
+  webDir: 'dist/indoor-plants',
   server: {
     androidScheme: 'https'
   }
